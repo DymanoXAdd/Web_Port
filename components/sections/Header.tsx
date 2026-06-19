e72ff27ba91bde6eda4ee3c2540c464b41ea3769
DymanoXAdd <Luisruiz1001@yahoo.com>
2026-06-19 15:11:27 -0500

    fix a nested anchor (SocialIcon swithc to svg)

--- components/sections/Header.tsx
+++ components/sections/Header.tsx
@@ -44,14 +44,21 @@ export default function Header({ socials }: Props) {
           {...slideInRight}
           className="flex flex-row items-center text-gray-300 cursor-pointer hover:text-green-400 transition-colors"
         >
-          <SocialIcon
-            className="cursor-pointer"
-            network="email"
-            fgColor="grey"
-            bgColor="transparent"
-            style={{ height: 40, width: 40 }}
-            url={"#contact"}
-          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-10 w-10 text-gray-500"
+            aria-hidden="true"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
+            />
+          </svg>
           <p className="uppercase hidden md:inline-flex text-sm text-gray-400 hover:text-green-400 transition-colors">
             Get in Touch
           </p>
